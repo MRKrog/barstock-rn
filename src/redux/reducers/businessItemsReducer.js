31a530f53262eff38112a66322e299a93b9fc403
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-05-24 16:10:55 -0600

    created action and reducer case to updating and adding alcohol to bus inventory Co-authored-by: Mackenzie Frey <42525195+Mackenzie-Frey@users.noreply.github.com> Co-authored-by: Julia Marco <35079289+juliamarco@users.noreply.github.com> Co-authored-by: Michael Krog <44214979+MRKrog@users.noreply.github.com>

--- src/redux/reducers/businessItemsReducer.js
+++ src/redux/reducers/businessItemsReducer.js
@@ -1,7 +1,18 @@
 export const businessItemsReducer = (state = [], action) => {
   switch(action.type){
     case "SET_BUSINESS_ITEMS":
-      return action.businessItems
+      return action.businessItems;
+    case "ADD_BUSINESS_ITEM":
+      return [...state, action.businessItems];
+    case "UPDATE_BUSINESS_ITEM":
+      const updatedBusinessItems = state.map(item => {
+        if(item.id === action.businessItems.id){
+          return action.businessItems
+        }else{
+          return item
+        }
+      })
+      return updatedBusinessItems;
     default:
       return state
   }
